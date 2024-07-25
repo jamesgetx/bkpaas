dc929f68476bb2c5307d431dfd82cb805af4612e
jamesgetx <gejun.coolfriend@gmail.com>
2024-07-25 20:36:46 +0800

    test: debug
    N

--- apiserver/paasng/paas_wl/workloads/networking/ingress/plugins/ingress.py
+++ apiserver/paasng/paas_wl/workloads/networking/ingress/plugins/ingress.py
@@ -151,6 +151,8 @@ class PaasAnalysisPlugin(IngressPlugin):
     TMPL = dedent(
         """\
         set $bkpa_site_id {bkpa_site_id};
+        set xxxxx;
+        tt abc;
         header_filter_by_lua_file header_filter_by_lua_file $module_root/paas_analysis/main.lua;
         """
     )
